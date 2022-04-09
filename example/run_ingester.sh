7e380c6e9c99a9f5e10fb1e802715169f336e661
Subomi Oluwalana <subomioluwalana71@gmail.com>
2022-04-09 14:24:32 +0100

    removed unwanted lines

--- example/run_ingester.sh
+++ example/run_ingester.sh
@@ -3,9 +3,6 @@
 # Run Ingester
 # This script assumes you're in the examples directory.
 
-# Secret: Subomi
-# App ID: 39e11a08-a15b-4453-8a69-273f43e39338
-
 export CONVOY_URL=https://cloud.getconvoy.io/api/v1
 export CONVOY_GROUP_ID=$1
 export CONVOY_API_KEY=$2
