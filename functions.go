d9380a7c5e059f034ae46eb0497b6f5919c05eea
Subomi Oluwalana <subomioluwalana71@gmail.com>
2022-04-11 22:05:37 +0100

    init pubsub function

--- functions.go
+++ functions.go
@@ -1,12 +1,16 @@
 package ingester
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/pubsub"
 	convoy "github.com/frain-dev/convoy-go"
 	convoyModels "github.com/frain-dev/convoy-go/models"
 	"github.com/go-chi/chi/v5"
@@ -16,8 +20,30 @@ var (
 	URL      = os.Getenv("CONVOY_URL")
 	USERNAME = os.Getenv("CONVOY_USERNAME")
 	PASSWORD = os.Getenv("CONVOY_PASSWORD")
+
+	// GOOGLE_CLOUD_PROJECT is a user-set environment variable.
+	projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
+
+	// Function topic
+	topic = os.Getenv("WEBHOOK_TOPIC")
+
+	// client is a global Pub/Sub client, initialized once per instance.
+	client *pubsub.Client
 )
 
+func init() {
+	// err is pre-declared to avoid shadowing client.
+	var err error
+
+	// client is initialized with context.Background() because it should
+	// persist between function invocations.
+	client, err = pubsub.NewClient(context.Background(), projectID)
+	if err != nil {
+		log.Fatalf("pubsub.NewClient: %v", err)
+	}
+}
+
+// WebhookEndpoint is a HTTP Function to receive events from the world.
 func WebhookEndpoint(w http.ResponseWriter, r *http.Request) {
 	// Build Router.
 	router := chi.NewRouter()
@@ -32,12 +58,32 @@ func WebhookEndpoint(w http.ResponseWriter, r *http.Request) {
 	router.ServeHTTP(w, r)
 }
 
+// PushToConvoy is a Pub/Sub Triggered Function to push events to Convoy.
+func PushToConvoy(ctx context.Context, m pubSubMessage) error {
+	payload := []byte(string(m.Data))
+	req := &convoyRequest{}
+	if err := req.FromBytes(payload); err != nil {
+		log.Printf("Failed to parse payload - %v", err)
+		return err
+	}
+
+	// Actual push to Convoy.
+	convoyClient := convoy.New()
+	_, err := convoyClient.CreateAppEvent(&req.Data)
+
+	if err != nil {
+		return errors.New(fmt.Sprintf("Server Error: Failed to send event to Convoy - %+v", err))
+	}
+
+}
+
 // HTTP Handlers
 
 // PaystackHandler ack webhooks from https://paystack.com
 // NEEDS:
 // CONVOY_PAYSTACK_APP_ID
 // PAYSTACK_SECRET
+// TODO(subomi): Handle response and response status code properly.
 func PaystackHandler(w http.ResponseWriter, r *http.Request) {
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -77,20 +123,34 @@ func PaystackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Push to Convoy.
-	// TODO(subomi): Build in some form of retries here to ensure reliability.
-	convoyClient := convoy.NewWithCredentials(URL, USERNAME, PASSWORD)
 	appID := os.Getenv("CONVOY_PAYSTACK_APP_ID")
-	_, err = convoyClient.CreateAppEvent(appID, &convoyModels.EventRequest{
-		Event: event.Event,
-		Data:  []byte(`<insert-real-data>`),
-	})
+	req := &convoyRequest{
+		AppID: appID,
+		Data: convoyModels.EventRequest{
+			Event: event.Event,
+			Data:  payload,
+		},
+	}
 
-	// TODO(subomi): This is critical. Add logs here.
+	data, err := req.ToBytes()
 	if err != nil {
-		w.Write([]byte("Server Error: Failed to send event to Convoy"))
+		log.Printf("Failed to transform to bytes")
+		w.Write([]byte(`Failed to transform bytes`))
 		return
 	}
 
+	m := &pubsub.Message{
+		Data: data,
+	}
+	id, err := client.Topic(topic).Publish(r.Context(), m).Get(r.Context())
+	if err != nil {
+		log.Printf("topic(%s).Publish.Get: %v", topic, err)
+		w.Write([]byte("Error publishing event"))
+		return
+	}
+	fmt.Fprintf(w, "Message published: %v", id)
+
+	w.Write([]byte("Event sent"))
 }
 
 // MonoHandler ack webhooks from https://mono.com
--- go.mod
+++ go.mod
@@ -3,19 +3,39 @@ module github.com/frain-dev/convoy-ingester
 go 1.18
 
 require (
+	cloud.google.com/go/pubsub v1.3.1
 	github.com/GoogleCloudPlatform/functions-framework-go v1.5.3
+	github.com/frain-dev/convoy-go v0.2.0
 	github.com/go-chi/chi/v5 v5.0.7
 )
 
 require (
+	cloud.google.com/go v0.100.2 // indirect
+	cloud.google.com/go/compute v1.3.0 // indirect
 	cloud.google.com/go/functions v1.0.0 // indirect
+	cloud.google.com/go/iam v0.1.0 // indirect
+	cloud.google.com/go/kms v1.4.0 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.6.1 // indirect
-	github.com/frain-dev/convoy-go v0.1.5 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/google/uuid v1.1.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/api v0.70.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
